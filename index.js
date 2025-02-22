e7de57c3bf5e6bfe081090d9b8ef53f40d2f6a39
Einzelgaanger <binfred.ke@gmail.com>
2025-02-22 15:02:50 +0300

    Updated 8 index css html js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// Hamburger Menu Toggle
-const hamburger = document.getElementById('hamburger');
-const nav = document.getElementById('nav');
-
-hamburger.addEventListener('click', () => {
-  nav.classList.toggle('active');
-  hamburger.classList.toggle('active');
-});
-
 // Contact Popup Toggle
 function toggleContactPopup() {
   const contactPopup = document.getElementById('contactPopup');
@@ -17,7 +8,7 @@ function toggleContactPopup() {
 document.addEventListener('click', (event) => {
   const contactPopup = document.getElementById('contactPopup');
   const contactForm = document.querySelector('.contact-form');
-  if (contactPopup.classList.contains('active') && !contactForm.contains(event.target) && !event.target.matches('.contact-btn')) {
+  if (contactPopup.classList.contains('active') && !contactForm.contains(event.target) && !event.target.matches('.cta-btn')) {
     contactPopup.classList.remove('active');
   }
 });
